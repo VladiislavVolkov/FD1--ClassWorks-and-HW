253eb7e1508fe827a80b0c941a263951111d7b75
VladiislavVolkov <kali.laskaa@gmail.com>
2022-09-28 21:19:35 +0300

    HM13

--- Homework 13/js/script.js
+++ Homework 13/js/script.js
@@ -1,16 +1,28 @@
-function reverseNumber(n) {
-    let a = String(n)
+function reverseNumber(num) {         // ПЕРЕВЕРНУТОЕ ЧИСЛО
+    let a = String(num)
     let result = '';
     for (let i = a.length-1; i >= 0; i--) {
         result += a[i];
-        console.log(result, i)
     }
     return result;
-
         };
     
 let c = reverseNumber(prompt('Введите число, которое хотите "перевернуть":'));
 alert(`"Перевернутое" число - ` + c);
 
+
+
+
+
+function pow(a,b) {                   // ЧИСЛО А В СТЕПЕНИ Б
+    let res = 1                    
+    for (let i = 0; i < b; i++){
+        res = res * a
+    }
+    return res;
+};
+
+let f = pow(prompt('Введите первое число А (которое возводится в степень)'),prompt('Введите второе число Б (которое будет степенью)'));
+alert(f)
 
 
