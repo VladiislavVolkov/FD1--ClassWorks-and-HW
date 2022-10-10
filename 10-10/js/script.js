9d134e3fa0d90d062e292414135857e3ca46c8d3
Vladislav Volkov <110058489+VladiislavVolkov@users.noreply.github.com>
2022-10-10 21:20:08 +0300

    Add files via upload

---
+++ 10-10/js/script.js
@@ -0,0 +1,52 @@
+// let a = 100.567548464
+
+// a.toFixed(2)
+// console.log(a.toFixed(2))
+
+// Number.isNaN(a)
+// Number.isFinite(a)
+
+// console.log(Number.isNaN())
+// console.log(Number.isFinite())
+
+// let b = " qwerty "
+// console.log(b.length)
+// console.log(b.charAt(2))
+// console.log(b.toLowerCase())
+// console.log(b.toUpperCase())
+// console.log(b.indexOf("t"))
+// console.log(b.includes("p"))
+// console.log(b.trim())
+
+
+// console.log(document.body.childNodes[3].style.backgroundColor = "red")
+
+
+// let node = document.getElementById("1").childNodes[7]
+// console.log(node.style.backgroundColor = "red")
+
+
+
+// let nodes = document.getElementsByClassName('2')
+
+// for ( node of nodes) {
+//     node.style.backgroundColor = "red"
+// }
+
+
+// let nodes = document.getElementsByTagName('div')
+
+// for ( node of nodes) {
+//     node.style.backgroundColor = "red"
+// }
+
+
+let node = document.querySelector('.as')
+console.log(node.innerHTML)
+
+
+
+
+
+
+
